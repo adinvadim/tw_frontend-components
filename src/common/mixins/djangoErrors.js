ee463122ebfa5bc36d1430163580d33cdd163f7e
tusaeff <tusaeff@yandex.ru>
2017-12-20 19:46:13 +0300

    DjangoErrorsMixin specs

--- src/common/mixins/djangoErrors.js
+++ src/common/mixins/djangoErrors.js
@@ -6,9 +6,9 @@ import fp from 'lodash/fp';
  * @param {Array} arr Array of field
  * @returns {Object}
  */
-const fieldsListToObject = fp.reduce((acc, e) => fp.assoc(e, null)(acc), {});
+export const fieldsListToObject = fp.reduce((acc, e) => fp.assoc(e, null)(acc), {});
 
-const toVuetifyRule = err => (fp.isNull(err) ? [] : err);
+export const toVuetifyRule = err => (fp.isNull(err) ? [] : err);
 
 /**
  * Convert django errors to vuetify text-field rules
@@ -16,7 +16,7 @@ const toVuetifyRule = err => (fp.isNull(err) ? [] : err);
  * @param {Object} errors Django errors dict
  * @return {Object}
  */
-const formErrors = errors => fp.mapValues(toVuetifyRule)(errors);
+export const formErrors = fp.mapValues(toVuetifyRule);
 
 export default function factory(fields) {
   let arrFields = fields;
---
+++ test/unit/specs/DjangoErrorMixin.spec.js
@@ -0,0 +1,74 @@
+/* eslint-disable */
+import Vue from 'vue';
+import { mount } from 'vue-test-utils';
+
+import DjangoErrorMixin, { toVuetifyRule, fieldsListToObject, formErrors } from '@/common/mixins/djangoErrors';
+
+const fields = ['fieldA', 'fieldB'];
+
+const template = {
+  mixins: [DjangoErrorMixin(fields)],
+  render(h) { return h() },
+}
+
+describe('DjangoErrorsMixin', () => {
+  it('toVuetifyRule', (done) => {
+    const res1 = toVuetifyRule('error');
+    const res2 = toVuetifyRule(null);
+
+    assert(res1 === 'error', 'shouldnt change origin error');
+    assert(Array.isArray(res2), 'should replace null with empty array');
+    done();
+  });
+
+  it('fieldListToObject', (done) => {
+    const test = ['test1', 'test2', 'test3'];
+    const res = fieldsListToObject(test);
+
+    assert(typeof res === 'object');
+    assert.deepEqual(res, { test1: null, test2: null, test3: null })
+    done();
+  });
+
+  it('formErrors', (done) => {
+    const test = { a: 'error', b: ['error', 'error'], c: null};
+    const res = formErrors(test);
+
+    assert.deepEqual(res, { ...test, c: [] });
+    done();
+  });
+
+  it('initialize', (done) => {
+    const wrapper = mount(template);
+
+    fields.forEach((item) => {
+      expect(wrapper.vm.errors).to.have.property(item)
+    });
+    done();
+  });
+
+  it('updateErrors', (done) => {
+    const wrapper = mount(template);
+    const test = { fieldA: 'error', fieldB: null};
+
+    wrapper.vm.updateErrors(test);
+    assert.deepEqual(wrapper.vm.errors, { ...test, fieldB: [] });
+    assert.deepEqual(wrapper.vm.djangoErrors, { ...test });
+
+    done();
+  });
+
+  it('clearErrors', (done) => {
+    const test = { fieldA: 'error', fieldB: null};
+    const wrapper = mount(template);
+
+    wrapper.vm.updateErrors(test);
+    wrapper.vm.clearErrors();
+
+    assert.deepEqual(wrapper.vm.errors, {fieldA: [], fieldB: []});
+    assert.deepEqual(wrapper.vm.djangoErrors, {fieldA: null, fieldB: null});
+    
+
+    done();
+  });
+});
